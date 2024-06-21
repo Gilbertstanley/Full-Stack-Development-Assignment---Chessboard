2ee44362cdb1c83410a22ace63b668aeb328d3bf
Gilbert Stanley <gilbert.stanley2k@gmail.com>
2024-06-21 17:00:30 +0530

    Chessboard

---
+++ logic.js
@@ -0,0 +1,41 @@
+
+//inserting the images
+function insertImage() {
+    document.querySelectorAll('.box').forEach(image => {
+        if (image.innerText.length !== 0) {
+            if (image.innerText == 'Wpawn' || image.innerText == 'Bpawn') {
+                image.innerHTML = `${image.innerText} <img class='all-img all-pown' src="${image.innerText}.png" alt="">`
+                image.style.cursor = 'pointer'
+            }
+            else {
+                image.innerHTML = `${image.innerText} <img class='all-img' src="${image.innerText}.png" alt="">`
+                image.style.cursor = 'pointer'
+            }
+        }
+    })
+}
+insertImage()
+
+//Coloring the board
+
+function coloring() {
+    const color = document.querySelectorAll('.box')
+
+    color.forEach(color => {
+        getId = color.id
+        arr = Array.from(getId)
+        arr.shift()
+        aside = eval(arr.pop())
+        aup = eval(arr.shift())
+        a = aside + aup
+
+        if (a % 2 == 0) {
+            color.style.backgroundColor = 'rgb(232 235 239)'
+        }
+        if (a % 2 !== 0) {
+            color.style.backgroundColor = 'rgb(125 135 150)'
+        }
+    })
+}
+coloring()
+
